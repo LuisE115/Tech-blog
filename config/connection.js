3fc999642f681d918067e875b8d06a52c2412f1d
kalho <luisenriquez11@hotmail.com>
2022-05-27 12:09:01 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+const sequelize = process.env.JAWSB_URL
+  ?new Sequelize(process.env.JAWSB_URL)
+  : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PW,
